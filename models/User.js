2313d7ea17ab7b9e50bb37b4cd03da07ab4b6210
Abigail LeConte <abigailleconte@Abigails-MBP.attlocal.net>
2022-10-21 13:46:25 -0400

    created app

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+        };
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: Datatypes.STRING
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        };
+    },
+    {
+        sequelize, 
+        timestamps: true,
+        freezeTableName: true, //by defalut, without true, table wtll be call users
+        modelName: "user",
+    }
+    );
